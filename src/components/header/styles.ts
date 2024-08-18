10937b3f53e541d2a088559ca099c0e8c41c2a66
Henrique <henriq2303@gmail.com>
2024-08-18 13:12:29 -0300

    Fist commit

---
+++ src/components/header/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import headerCover from "../../assets/cover.png"
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  padding-top: 4rem;
+  padding-bottom: 8.375rem;
+  justify-content: center;
+  height: 296px;
+  width: 100%;
+  background-image: url(${headerCover});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,26 @@
+import { ArrowSquareOut, Buildings, GithubLogo, Users } from "phosphor-react";
+import { FooterProfile, HeaderProfile, InfoProfile, ProfileContainer } from "./styles";
+
+export function Home() {
+  return (
+    <ProfileContainer>
+      <img src="https://github.com/HenriqueSC23.png" alt="" />
+      <InfoProfile>
+        <HeaderProfile>
+          <h1>Henrique</h1>
+          <a href="#">GITHUB <ArrowSquareOut size={12} /></a>
+        </HeaderProfile>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, voluptates nemo.
+          Beatae quam odit aliquam accusamus minima asperiores facere, quidem libero veniam molestiae ad quae,
+          dignissimos in labore architecto officiis?
+        </p>
+        <FooterProfile>
+          <div><GithubLogo weight="fill" size={18} />HenriqueSC23 </div>
+          <div><Buildings weight="fill" size={18} />Vou para Praia</div>
+          <div><Users weight="fill" size={18} />10 seguidores</div>
+        </FooterProfile>
+      </InfoProfile>
+    </ProfileContainer>
+  )
+}
